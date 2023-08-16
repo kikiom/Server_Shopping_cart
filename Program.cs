519b91b3d021057df9b92e5d5a7099e1b9d85e25
zlati <zlati@DESKTOP-L4VCME9>
2023-08-16 14:08:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Shopping_cart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
